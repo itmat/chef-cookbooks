e04acaaa56d16a8248c16f5203320fe2b3581c2b
Angel Pizarro <angel@upenn.edu>
2010-08-05 17:53:11 +0000

    added GD2 recipe

---
+++ gd2/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "ITMAT UPENN"
+maintainer_email "itmat.apps@itmat.upenn.edu"
+license          "Apache 2.0"
+description      "Installs/Configures libgd2 tools, libraries and development libraries"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1"
+
+supports "ubuntu"
