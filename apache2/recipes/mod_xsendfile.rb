cfa12e4228a0c1f9cca107a557c8fcd99d1d9239
Angel Pizarro <angel@upenn.edu>
2010-08-16 19:26:37 +0000

    updated mascot deps and apache recipes

--- apache2/recipes/mod_xsendfile.rb
+++ apache2/recipes/mod_xsendfile.rb
@@ -1,10 +1,12 @@
-# Chef recipe to install mod_xsendfile to Ubuntu i386.
+Chef recipe to install mod_xsendfile to Ubuntu i386.
 remote_file "/tmp/mod_xsendfile.c" do
   source "mod_xsendfile.c"
 end
 
-ruby_block do
-  system("cd tmp; apxs -cia mod_xsendfile.c")
+bash "compile_xsendfile" do 
+  user "root"
+  cwd "/tmp"
+  code "apxs -cia mod_xsendfile.c"
 end
 
 apache_module "xsendfile" do
--- mascot_dependencies/metadata.rb
+++ mascot_dependencies/metadata.rb
@@ -1,11 +1,12 @@
 maintainer       "ITMAT UPENN"
 maintainer_email "itmat.apps@itmat.upenn.edu"
 license          "Apache 2.0"
-description      "Installs Perl CPAN modules to support Mascot Server"
+description      "Installs dependencies for a Mascot Server"
 version          "0.1"
 
 %w{ centos redhat suse fedora ubuntu debian }.each do |os|
   supports os
 end
-
 depends "perl"
+depends "gd2"
+depends "htdig"
